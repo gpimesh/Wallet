810ef5a5fa8ca8d65690a88b89672cf077915d48
gpimesh <79093134+gpimesh@users.noreply.github.com>
2022-01-25 18:17:08 +0100

    updated schema file.

---
+++ LeoVegas-wallet/src/main/resources/schema.sql
@@ -0,0 +1,51 @@
+drop table if exists account CASCADE ;
+drop table if exists transactions CASCADE ;
+drop table if exists users CASCADE;
+drop sequence if exists hibernate_sequence;
+
+create table ACCOUNT (
+ 					  id long AUTO_INCREMENT not null primary key,
+ 					  cust_account_number bigint (19) not null UNIQUE,  
+                      balance float (20) not null,
+                      account_owner  varchar_ignorecase (25) not null,
+                      creation_date timestamp,
+                      modified_date timestamp,
+                      creation_user varchar_ignorecase (25) not null,
+                      optlock long not null
+);
+CREATE SEQUENCE HIBERNATE_SEQUENCE START WITH 1 INCREMENT BY 1;
+CREATE INDEX cust_account_number ON ACCOUNT(cust_account_number);
+
+create table USERS (    
+						id long  not null primary key,
+						user_id varchar_ignorecase(25) not null UNIQUE,
+                       	first_name varchar (20) not null,
+                        last_name varchar (30) not null,
+                        email varchar_ignorecase (40) not null,
+                        creation_date timestamp,
+                        modified_date timestamp,
+                        account_no  bigint (19) not null UNIQUE,
+                        optlock long not null,
+                        
+                        constraint fk_customer_account foreign key(account_no) references ACCOUNT(cust_account_number)
+  
+);
+--CREATE SEQUENCE HIBERNATE_SEQUENCE START WITH 1 INCREMENT BY 1;
+CREATE INDEX user_id ON USERS(user_id);
+                           
+create table TRANSACTIONS (
+						id long AUTO_INCREMENT not null primary key,
+						transaction_id bigint (19) UNIQUE,
+						account_number bigint (19) not null, 
+						amount float,
+						creation_date timestamp,
+						modified_date timestamp,
+						creation_userid varchar_ignorecase(25),
+						optlock long not null,
+						transaction_type varchar,
+	constraint fk_account_transaction foreign key(account_number) references ACCOUNT(cust_account_number)				
+);
+
+CREATE INDEX transaction_id ON TRANSACTIONS(transaction_id);
+
+
